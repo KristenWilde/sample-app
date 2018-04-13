e993297ab1b459d7ea0a33d4bc2b999c91897ab0
Kristen Wilde <kristen.wilde@gmail.com>
2018-04-13 15:25:05 -0400

    add bootstrap css, refactor layout templates

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,30 +4,31 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get root_url
     assert_response :success
+    assert_select "title", "Sample App"
   end
 
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | SampleApp"
+    assert_select "title", "Sample App"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | SampleApp"
+    assert_select "title", "Help | Sample App"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | SampleApp"
+    assert_select "title", "About | Sample App"
   end
 
   test "shoud get contact" do
     get static_pages_contact_url
     assert_response :success
-    assert_select "title", "Contact | SampleApp"
+    assert_select "title", "Contact | Sample App"
   end
 
 end
