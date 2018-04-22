72580e1739373d8e05e970b8c8749a712e0fe34d
Kristen Wilde <kristen.wilde@gmail.com>
2018-04-21 21:25:35 -0400

    implement index page and tests for it

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,9 @@
 module UsersHelper
 
-  def gravatar_for(user)
+  def gravatar_for(user, size: 80)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 
 end
-
-# juli = {name: 'Jules', email: 'example@railstutorial.org'}
-
-# p UsersHelper::gravatar_for(juli)
