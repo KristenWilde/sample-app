e9bec41b21bd39a4e7ef24e3a90fd8860542594a
Kristen Wilde <kristen.wilde@gmail.com>
2018-04-22 21:39:08 -0400

    Finish user index and destroy actions with tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,14 +2,14 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @user       = users(:juli)
+    @user       = users(:juli) # is admin
     @other_user = users(:michael)
   end
 
   test "should get new user page" do
     get signup_path
     assert_response :success
-    assert_select "title", "Sign up | Sample App" 
+    assert_select "title", "Sign up | Sample App"
   end
 
   test "should redirect edit when not logged in" do
@@ -30,4 +30,28 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as @other_user
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+                                    user: { password: 'password',
+                                            password_combination: 'password',
+                                            admin: true                       } }
+    assert_not @other_user.reload.admin?
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
 end
